ab06ad39efa821f8f714180b9186c793702a6bf9
João Penedos <a23497@alunos.ipca.pt>
2022-12-06 18:55:19 +0000

    Create class - Utilizador, Pessoa, Funcionario

---
+++ WinFormsApp1/ProjectPOO.Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPOO.Models
+{
+    public class Pessoa
+    {
+        public Pessoa(string email, string nome, int numeroTelemovel)
+        {
+            this.Email = email;
+            this.Nome = nome;
+            this.NumeroTelemovel = numeroTelemovel;
+        }
+
+        public string Email { get; set; }
+
+        public string Nome { get; set; }
+
+        public int NumeroTelemovel { get; set; }
+    }
+}
